fd89fdd53d975137a8caad3484830fe18811fb4b
Carlos Saritama <cssaritama@gmail.com>
2025-04-15 23:19:33 +0200

    Update nyc_taxi_pipeline.py

--- dags/nyc_taxi_pipeline.py
+++ dags/nyc_taxi_pipeline.py
@@ -5,22 +5,24 @@ from datetime import datetime
 default_args = {
     'owner': 'cssaritama',
     'start_date': datetime(2023, 1, 1),
-    'vars': {
-        'data_lake_bucket': 'nyc-taxi-data-lake-2025-cssaritama'
-    }
+    'retries': 2
 }
 
 with DAG(
-    'nyc_taxi_pipeline',
+    'nyc_taxi_analytics',
     default_args=default_args,
-    schedule_interval='@monthly',
+    schedule_interval='0 3 * * *',  # Runs daily at 3 AM
+    max_active_runs=1
 ) as dag:
 
     ingest_to_bq = GCSToBigQueryOperator(
-        task_id='ingest_to_bq',
-        bucket='{{ var.value.data_lake_bucket }}',
+        task_id='load_daily_trips',
+        bucket='nyc-taxi-data-lake-2025-cssaritama',
         source_objects=['raw/yellow_tripdata_2023-*.parquet'],
-        destination_project_dataset_table='nyc_taxi_analytics.raw_rides',
+        destination_project_dataset_table='nyc_taxi_prod.raw_trips',
         source_format='PARQUET',
         write_disposition='WRITE_TRUNCATE',
+        time_partitioning={'type': 'DAY'},
+        cluster_fields=['PULocationID'],
+        dag=dag
     )
