05a8e77b30373c89acb0794813bd8187750dc045
Carlos Saritama <cssaritama@gmail.com>
2025-04-15 23:08:31 +0200

    Update nyc_taxi_pipeline.py

--- dags/nyc_taxi_pipeline.py
+++ dags/nyc_taxi_pipeline.py
@@ -6,7 +6,7 @@ default_args = {
     'owner': 'cssaritama',
     'start_date': datetime(2023, 1, 1),
     'vars': {
-        'data_lake_bucket': 'nyc-taxi-data-lake-2025'
+        'data_lake_bucket': 'nyc-taxi-data-lake-2025-cssaritama'
     }
 }
 
@@ -19,8 +19,8 @@ with DAG(
     ingest_to_bq = GCSToBigQueryOperator(
         task_id='ingest_to_bq',
         bucket='{{ var.value.data_lake_bucket }}',
-        source_objects=['raw/yellow_tripdata_2023-*.parquet'], 
-        destination_project_dataset_table='nyc_taxi.raw_rides',
+        source_objects=['raw/yellow_tripdata_2023-*.parquet'],
+        destination_project_dataset_table='nyc_taxi_analytics.raw_rides',
         source_format='PARQUET',
         write_disposition='WRITE_TRUNCATE',
     )
