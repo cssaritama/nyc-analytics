c33146830b3d8d213ad08d3fd9f3a50a678df8d4
Carlos Saritama <cssaritama@gmail.com>
2025-04-15 23:20:07 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,9 +7,11 @@ resource "google_storage_bucket" "data_lake" {
   name          = "nyc-taxi-data-lake-2025-cssaritama"
   location      = "US"
   force_destroy = true
+  uniform_bucket_level_access = true
 }
 
 resource "google_bigquery_dataset" "nyc_taxi" {
-  dataset_id = "nyc_taxi_analytics"
-  location   = "US"
+  dataset_id    = "nyc_taxi_prod"
+  friendly_name = "NYC Taxi Analytics"
+  location      = "US"
 }
